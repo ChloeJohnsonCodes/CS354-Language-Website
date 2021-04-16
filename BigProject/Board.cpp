72e6443bc412f7634af975e1184fd6aab32103f8
Chinwendumnjoku97 <Chinwendumnjoku@u.boisestate.edu>
2021-04-16 08:57:14 -0600

    IfWinner method partially completed

--- BigProject/Board.cpp
+++ BigProject/Board.cpp
@@ -1,25 +1,43 @@
 /* Board class definitions
  */
- 
+
  #include "Board.h"
  #include "Box.h"
- 
+
  Board::Board(){
     Box boardArray[9];
  }
- 
+
  int Board::ClearBoard(){
-     
+
  }
- 
- bool Board::IfWinner(){
-     
+
+ bool Board::IfWinner()
+ {
+     bool winner = false;
+
+     //checks the rows
+     if((board[0]==board[1]) && (board[1]==board[2]) && board[0] != 0)
+     {
+         winner = true;
+     }
+     else if ((board[3] == board[4]) && (board[4] == board[5]) && board[3] != 0)
+     {
+         winner = true;
+     }
+
+     else if ((board[6] == board[7]) && (board[7] == board[8]) && board[6] != 0)
+     {
+         winner = true;
+     }
+
+
  }
- 
+
  bool Board::IfFilled(){
-     
+
  }
- 
+
  void Board::PrintBoard(){
      std::cout<<" "<<boardArray[0].PrintBox() << " | " << boardArray[1].PrintBox << " | " << boardArray[2].PrintBox << "\n";
      std::cout<<"---+---+---\n";
